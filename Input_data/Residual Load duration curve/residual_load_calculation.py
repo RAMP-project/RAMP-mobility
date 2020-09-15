e9d6ddaf2f4e9497e591b168bca0be07d4403055
Andrea Mangipinto <andrea.mangipinto@gmail.com>
2020-09-15 12:11:25 +0200

    Added residual load files to compute RES Integration charging strategy

--- Input_data/Residual Load duration curve/residual_load_calculation.py
+++ Input_data/Residual Load duration curve/residual_load_calculation.py
@@ -8,34 +8,42 @@ Created on Tue May 26 14:56:07 2020
 import pandas as pd
 import pytz
 
-inputfolder = r"C:\Users\Andrea\OneDrive - Politecnico di Milano\Università\Tesi (OneDrive)\Database Improvements\3. EV profiles\Residual Load duration curve"
-
-inputfile_pv = f"{inputfolder}/ninja_pv_europe_v1.1_merra2.csv"
-inputfile_wind = f"{inputfolder}/ninja_wind_europe_v1.1_current_national.csv"
-inputfile_cap = f"{inputfolder}/TIMES_Capacities_technology_2050.csv"
-inputfile_load = f"{inputfolder}/time_series_60min_singleindex_filtered.csv"
-
-output_folder = r'C:\Users\Andrea\OneDrive - Politecnico di Milano\Università\Tesi (OneDrive)\Database Improvements\3. EV profiles\Residual Load duration curve'
+inputfile_pv = f"ninja_pv_europe_v1.1_merra2.csv"
+inputfile_wind = f"ninja_wind_europe_v1.1_current_national.csv"
+inputfile_cap = f"TIMES_Capacities_technology_2050.csv"
+inputfile_load = f"time_series_60min_singleindex_filtered.csv"
 
 def residual_load(country, year, load_multiplier, inputfile_load, inputfile_pv, inputfile_wind , inputfile_cap):
     
+        
+    if country == 'EL':
+        country_tz = 'GR'
+    elif country == 'UK':
+        country_tz = 'GB'
+    else:
+        country_tz = country
+    
     # Solar AF 
     pv_af = pd.read_csv(inputfile_pv, index_col = 0) #Read the input file
+    pv_af.rename(columns={"GB": "UK", "GR": "EL"}, inplace = True)
     pv_af = pd.DataFrame(pv_af[country]) #Filter only for needed country
     ind_init = pd.date_range(start=pv_af.index[0], end=pv_af.index[-1], freq='H', tz = 'UTC')
     pv_af.set_index(ind_init, inplace = True) #Set index to datetime
-    pv_af_tz = pv_af.tz_convert(pytz.country_timezones[country][0]) # Convert to country timezone
+
+    pv_af_tz = pv_af.tz_convert(pytz.country_timezones[country_tz][0]) # Convert to country timezone
 
     pv_af_loc = pv_af_tz.tz_localize(None, ambiguous = 'NaT') # Remove the timezone information (local time)
     pv_af_loc = pv_af_loc[~pv_af_loc.index.duplicated(keep='first')] # Remove duplicate hours arising from tz conversion
 
     # Wind AF 
     wind_af = pd.read_csv(inputfile_wind, index_col = 0) #Read the input file
+    wind_af.rename(columns={"GB": "UK", "GR": "EL"}, inplace = True)
+
     wind_af = pd.DataFrame(wind_af[country]) #Filter only for needed country
     ind_init = pd.date_range(start=wind_af.index[0], end=wind_af.index[-1], freq='H', tz = 'UTC')
     wind_af.index = pd.to_datetime(wind_af.index, utc = True)
     wind_af = wind_af.reindex(ind_init) #Set index to datetime
-    wind_af_tz = wind_af.tz_convert(pytz.country_timezones[country][0]) # Convert to country timezone
+    wind_af_tz = wind_af.tz_convert(pytz.country_timezones[country_tz][0]) # Convert to country timezone
 
     wind_af_loc = wind_af_tz.tz_localize(None, ambiguous = 'NaT') # Remove the timezone information (local time)
     wind_af_loc = wind_af_loc[~wind_af_loc.index.duplicated(keep='first')] # Remove duplicate hours arising from tz conversion
@@ -63,7 +71,7 @@ def residual_load(country, year, load_multiplier, inputfile_load, inputfile_pv, 
     load = pd.DataFrame(load[country])
     ind_init = pd.date_range(start= load.index[0], end=load.index[-1], freq='H', tz = 'UTC')
     load.set_index(ind_init, inplace = True)
-    load_tz = load.tz_convert(pytz.country_timezones[country][0])
+    load_tz = load.tz_convert(pytz.country_timezones[country_tz][0])
     
     load_loc = load_tz.tz_localize(None, ambiguous = 'NaT') # Remove the timezone information (local time)
     load_loc = load_loc[~load_loc.index.duplicated(keep='first')] # Remove duplicate hours arising from tz conversion
@@ -86,10 +94,14 @@ def residual_load(country, year, load_multiplier, inputfile_load, inputfile_pv, 
             
     return res_load
 
-country = 'IT'
-load_multiplier = 2.5
-year = 2016
+countries = ['AT', 'BE', 'BG', 'CH', 'CZ', 'DE', 'DK', 'EE', 'EL', 'ES', 'FI', 'FR', 'HR', 'HU',
+  'IE', 'IT','LT', 'LU','LV', 'NL', 'NO', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK', 'UK']
 
-res_load = residual_load(country, year, load_multiplier, inputfile_load, inputfile_pv , inputfile_wind, inputfile_cap)
-
-res_load.to_csv(f'{output_folder}/residual_load_{country}.csv')
+for c in countries: 
+    country = f'{c}'
+    load_multiplier = 2.5
+    year = 2016
+    
+    res_load = residual_load(country, year, load_multiplier, inputfile_load, inputfile_pv , inputfile_wind, inputfile_cap)
+    
+    res_load.to_csv(f'residual_load_{country}_{year}.csv')
